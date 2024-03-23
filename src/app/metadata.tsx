a26c372c1ac976c2609a7ba61c317b9ca3d56198
Tyler Weeres <tweeres04@gmail.com>
2024-03-22 20:45:36 -0700

    Apple web app bullshit

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -3,7 +3,23 @@ import { Metadata } from 'next'
 export const explanation =
 	'A game is marked close if it has less than 5 minutes left and the teams are less than 10 points apart.'
 
+const title = 'ncaa close games'
+const imageUrl = '/basketball.png'
+
 export const metadata: Metadata = {
-	title: 'ncaa close games',
+	title,
 	description: `A quick way to see which March Madness games are good. ${explanation}`,
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: 'default',
+		startupImage: {
+			url: imageUrl,
+		},
+		title,
+	},
+	icons: {
+		apple: {
+			url: imageUrl,
+		},
+	},
 }
