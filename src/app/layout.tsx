5602f34fa1ed2703240d16a42b6f0a471ff468cb
Tyler Weeres <tweeres04@gmail.com>
2024-03-24 17:27:00 -0700

    Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 export { metadata } from './metadata'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 import './globals.css'
 
@@ -13,6 +14,9 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
+			{process.env.NODE_ENV === 'production' ? (
+				<GoogleAnalytics gaId="G-2W4G5BP582" />
+			) : null}
 		</html>
 	)
 }
