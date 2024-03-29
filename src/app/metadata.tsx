7640e2f59684b47ccde27422890d02c336ca6563
Tyler Weeres <tweeres04@gmail.com>
2024-03-29 07:45:37 -0700

    Add an og image

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -4,11 +4,12 @@ export const explanation =
 	'A game is marked close if it has less than 5 minutes left and the teams are less than 10 points apart.'
 
 const title = 'ncaa close games - March Madness tracker'
+const description = `A quick way to track close games and upsets. ${explanation}`
 const imageUrl = '/basketball.png'
 
 export const metadata: Metadata = {
 	title,
-	description: `A quick way to track close games and upsets. ${explanation}`,
+	description,
 	appleWebApp: {
 		capable: true,
 		statusBarStyle: 'default',
@@ -21,5 +22,10 @@ export const metadata: Metadata = {
 		apple: {
 			url: imageUrl,
 		},
+	},
+	openGraph: {
+		title,
+		description,
+		images: 'https://ncaa-close-games.tweeres.ca/og-image.jpg',
 	},
 }
