a22ba52a342b584d9ba890f32310a113e823828f
Tyler Weeres <tweeres04@gmail.com>
2024-05-05 14:40:54 -0700

    Set canonical urls

--- src/app/[gender]/page.tsx
+++ src/app/[gender]/page.tsx
@@ -1,1 +1,16 @@
+import { Metadata } from 'next'
+import { Gender } from '../models'
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { gender: Gender }
+}): Promise<Metadata> {
+	return {
+		alternates: {
+			canonical: `https://ncaa-close-games.tweeres.ca/${params.gender}`,
+		},
+	}
+}
+
 export { default } from '../CloseGamesServer'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,7 @@
+import { Metadata } from 'next'
+
 export { default } from './CloseGamesServer'
+
+export const metadata: Metadata = {
+	alternates: { canonical: 'https://ncaa-close-games.tweeres.ca/men' },
+}
