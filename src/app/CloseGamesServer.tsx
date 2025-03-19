00466d00fb435458b158bb3d67d1e6f2dff41f9f
Tyler Weeres <tweeres04@gmail.com>
2025-03-19 15:50:31 -0700

    Make it work for this year

--- src/app/CloseGamesServer.tsx
+++ src/app/CloseGamesServer.tsx
@@ -19,14 +19,22 @@ type WomenScoresResponse = {
 	games: { game: WomenGame }[]
 }
 
+function getNow() {
+	return process.env.NODE_ENV === 'production'
+		? new Date(Date.now() - 1000 * 60 * 60 * 7) // This isn't pretty, but it might always work since MM should always be during DST
+		: new Date()
+}
+
 const scoresUrls = {
-	men: () =>
-		'https://sdataprod.ncaa.com/?variables=%7B%22seasonYear%22%3A2023,%22current%22%3Atrue%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1,%22sha256Hash%22%3A%222d9054b672f94e541c1de408ab4af3c6d014ba37915a58eca97b8198bcc198da%22%7D%7D',
+	men: () => {
+		const now = getNow()
+		const year = now.getFullYear()
+		return `https://sdataprod.ncaa.com/?variables=%7B%22seasonYear%22%3A${
+			year - 1
+		},%22current%22%3Atrue%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1,%22sha256Hash%22%3A%222d9054b672f94e541c1de408ab4af3c6d014ba37915a58eca97b8198bcc198da%22%7D%7D`
+	},
 	women: () => {
-		const now =
-			process.env.NODE_ENV === 'production'
-				? new Date(Date.now() - 1000 * 60 * 60 * 7) // This isn't pretty, but it might always work since MM should always be during DST
-				: new Date()
+		const now = getNow()
 		const year = now.getFullYear()
 		const month = (now.getMonth() + 1).toString().padStart(2, '0')
 		const day = now.getDate().toString().padStart(2, '0')
